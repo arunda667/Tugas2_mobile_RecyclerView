6a296f058b6d695ff9730448f9ac07617a910b72
arunda667 <arundaengga07@gmail.com>
2022-11-01 20:01:30 +0700

    Tugas2_mobile_RecyclerView

---
+++ app/src/main/res/drawable-v24/RecycleView/app/src/main/java/com/example/pertemuan5/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pertemuan5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recylerView;
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.cenil,R.drawable.dadar_gulung,R.drawable.klepon,R.drawable.martabak};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.makanan);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        LaptopAdapter appAdapter = new
+                LaptopAdapter(this,s1,s2,s3,images);
+        recylerView.setAdapter(appAdapter);
+//        recylerView.setLayoutManager(new
+//                LinearLayoutManager(this));
+        LinearLayoutManager horizontalLayoutManagaer = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recylerView.setLayoutManager(horizontalLayoutManagaer);
+    }
+}
